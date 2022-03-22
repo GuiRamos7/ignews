aebabb915602575d8b9e264fa64cc206f8b3d018
Guilherme Ramos <guiramos@DESKTOP-LO9LDT6.localdomain>
2022-03-22 19:39:06 -0300

    inital commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,21 @@
+import Document, { Main, Head, NextScript, Html } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&family=Shippori+Antique+B1&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
