4e7d085ab6456bc6f985b83d5491fd7a6e35d2a3
Guilherme Ramos <guiramos@DESKTOP-LO9LDT6.localdomain>
2022-04-08 05:00:07 -0300

    adding rule to the user not subscribe again if he has a subscription, also adding activeSubscription on session object

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -44,6 +44,15 @@ export const getServerSideProps: GetServerSideProps = async ({
 }) => {
   const session = await getSession({ req });
 
+  if (!session?.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
   const prismic = getPrismicClient(req);
 
   const response = await prismic.getByUID('post', String(params?.slug), {});
